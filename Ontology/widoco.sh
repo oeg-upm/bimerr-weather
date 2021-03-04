f07759d435332e9aeddbd44497673633b674e49e
Salva5297 <salgonger1997@gmail.com>
2021-03-04 11:26:45 +0100

    Changed almost all files

--- Ontology/widoco.sh
+++ Ontology/widoco.sh
@@ -6,18 +6,11 @@ arr=()
 contador=0
 
 
-if [ $num_args -le 1 ]; then
+if [ $num_args -lt 1 ]; then
     echo "Not enough arguments. Type sh widoco.sh -h || --help to get the usage of this application."
 
-elif [ $num_args -lt 3 ]; then # mirar a ver si lo que recibe es una uri o un fichero
-    curl --location --request POST 'http://127.0.0.1:5000/' --header 'Content-Type: multipart/form-data; boundary=--------------------------554657334890040702125867' --form 'data={"getOntologyMetadata" : "","rewriteAll" : "","lang" : "en"}' --form 'ontoFile=@'$2 --output WidocoDocs.zip
-
-else
-    for arg in ${array[@]};
-    do
-		contador=$contador+1
-        if [ "$arg" = "--help" ] || [ "$arg" = "-h" ]; then
-            echo " "
+elif [ ${array[@]}="-h" ] || [ ${array[@]}="--help" ]; then
+	echo " "
             echo "Usage: sh widoco.sh + arguments (arguments below)"
             echo " "
             echo "-ontoFile ontologyFile            -includeImportedOntologies"
@@ -26,16 +19,24 @@ else
             echo "-oops                             -licensius"
             echo "-rewriteAll                       -ignoreIndividuals"
             echo "-crossRef                         -analytics analyticsFile"
-            echo "-saveConfig saveFile     -doNotDisplaySerializations"
+            echo "-saveConfig saveFile     	  -doNotDisplaySerializations"
             echo "-usecustomStyle                   -displaydirectImportsOnly"
             echo "-lang lang1-lang2                 -rewriteBase baseFile"
             echo "-excludeIntroduction              -uniteSections"
             break
 
-        elif [ "$arg" = "-ontoFile" ]; then
+elif [ $num_args -lt 3 ]; then # mirar a ver si lo que recibe es una uri o un fichero
+    curl --location --request POST 'http://widoco.linkeddata.es/' --header 'Content-Type: multipart/form-data; boundary=--------------------------554657334890040702125867' --form 'data={"getOntologyMetadata":"","rewriteAll":"","lang":"en"}' --form 'ontoFile=@'$2 --output WidocoDocs.zip
+
+else
+    for arg in ${array[@]};
+    do
+		contador=$contador+1
+
+        if [ "$arg" = "-ontoFile" ]; then
             ontologyFile="${array[$arg+$contador]}" # Get the file of the ontology
             # Add here the data of the ontology and de command
-            arr=("ontoFile:@$ontologyFile" "${arr[@]}")
+            arr=("ontoFile=@$ontologyFile" "${arr[@]}")
         
         elif [ "$arg" = "-ontoUri" ]; then
             ontologyUri="${array[$arg+$contador]}" # Get the uri of the ontology
@@ -104,15 +105,20 @@ else
             continue
         fi
     done
-    
+
 	data=${arr[0]}
-	unset 'arr[0]'
-	json=("${arr[@]}")
-	json=$(printf ",%s" "${json[@]}")
-	json=${json:1}
+	arrIN=(${data//=/ })
+	if [ $arrIN == "ontoFile" ]; then
+		unset 'arr[0]'
+		json=("${arr[@]}")
+		json=$(printf ",%s" "${json[@]}")
+		json=${json:1}
 	
-	echo curl --location --request POST "'"'http://127.0.0.1:5000/'"'" --header "'"'Content-Type: multipart/form-data; boundary=--------------------------554657334890040702125867'"'" --form "'"'data={'"$json"'}'"'" --form "'""$data""'" --output WidocoDocs.zip
-	
-
-
+		curl --location --request POST 'http://widoco.linkeddata.es/' --header 'Content-Type: multipart/form-data; boundary=--------------------------554657334890040702125867' --form 'data={'$json'}' --form $data --output WidocoDocs.zip
+	else
+		json=("${arr[@]}")
+		json=$(printf ",%s" "${json[@]}")
+		json=${json:1}
+		curl --location --request POST 'http://widoco.linkeddata.es/' --header 'Content-Type: multipart/form-data; boundary=--------------------------554657334890040702125867' --form 'data={'$json'}' --output WidocoDocs.zip
+	fi
 fi
